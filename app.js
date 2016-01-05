13326991c61e61612f441108a97a2f883856312c
kairath <thekairath@hotmail.com>
2016-01-05 14:13:36 +0000

    twitter login works

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-require("dotenv").load();
-require("./models/Users");
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var twitterStrategy = require('passport-twitter').Strategy;
+var mongoose = require('mongoose');
+require('./models/Users');
+require('dotenv').load();
+
+mongoose.connect(process.env.mongo_uri)
+//mongoose.connect('mongodb://localhost/images') //Local testing DB path
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -19,13 +25,17 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(require('express-session')({ secret: 'akjsecretaskdş', resave: true, saveUninitialized: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', routes);
 app.use('/users', users);
+
+var User = mongoose.model('User');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,7 +44,34 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
+//twitter login
+passport.use(new twitterStrategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: 'https://pinterest-clone-kairath.c9users.io/auth/twitter/callback'
+  },
+  function(token, tokenSecret, profile, cb) {
+    User.findOne({username: profile.screen_name}, function(err, response) {
+      if(err){ return; }
+        if(!response) {
+            var person = new User();
+            person.username = profile.screen_name;
+                
+            person.save();
+        }
+    })
+    return cb(null, profile);
+  }));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
 
 // development error handler
 // will print stacktrace
